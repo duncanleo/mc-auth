1da04100b502c97bbbeddc2a56cdb34eafa3217c
Duncan Leo <hello@duncanleo.me>
2019-09-29 19:08:02 +0800

    add basic event registration

--- src/main/java/me/duncanleo/spigot_plugin_base/main.kt
+++ src/main/java/me/duncanleo/spigot_plugin_base/main.kt
@@ -1,9 +1,13 @@
 package me.duncanleo.spigot_plugin_base
 
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.event.Listener
 
 class App : JavaPlugin() {
+class App : JavaPlugin(), Listener {
   override fun onEnable() {
     logger.info("Hello there!")
+
+    server.pluginManager.registerEvents(this, this)
   }
 }
